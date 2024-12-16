db8464486251757a8fa6defb5517eb245b062e60
Davide De Benedittis <davide.debenedittis@gmail.com>
2024-12-16 17:38:33 +0100

    Learning converges

--- my_rl/cluttered_env.py
+++ my_rl/cluttered_env.py
@@ -1,6 +1,5 @@
 from typing import Any, Dict, Optional, Tuple, Union
 
-
 import chex
 from flax import struct
 import jax
@@ -9,8 +8,9 @@ import jax.numpy as jnp
 from gymnax.environments import environment
 from gymnax.environments import spaces
 
-
-N_OBS = 10
+N_ACTIONS = 2
+N_OBSERVATIONS = 5
+N_OBSTACLES = 10
 
 
 @struct.dataclass
@@ -19,6 +19,7 @@ class EnvState(environment.EnvState):
     y: jnp.ndarray
     theta: jnp.ndarray
     obs_state: jnp.ndarray
+    target_state: jnp.ndarray
     time: int
 
 
@@ -29,14 +30,14 @@ class EnvParams(environment.EnvParams):
     min_u: Tuple[float, float] = (-1.0, -1.0)
     max_u: Tuple[float, float] = (1.0, 1.0)
     dt: float = 0.1
-    max_steps_in_episode: int = 500  # v0 had only 200 steps!
+    max_steps_in_episode: int = 256  # v0 had only 200 steps!
 
 
 class ClutteredEnv(environment.Environment[EnvState, EnvParams]):
     def __init__(self):
         super().__init__()
-        self.obs_shape =  (5,)
-        
+        self.obs_shape = (N_OBSERVATIONS,)
+    
     @property
     def default_params(self) -> EnvParams:
         return EnvParams()
@@ -49,25 +50,48 @@ class ClutteredEnv(environment.Environment[EnvState, EnvParams]):
         params: EnvParams,
     ) -> Tuple[chex.Array, EnvState, jnp.ndarray, jnp.ndarray, Dict[Any, Any]]:
         """Performs step transitions in the environment."""
-        
-        prev_terminal = self.is_terminal(state, params)
-        
-        action = jnp.clip(
-            action,
-            jnp.array(self.default_params.min_u),
-            jnp.array(self.default_params.max_u),
-        )
-        
+        # Robot state evolution
         x = state.x + action[0] * jnp.cos(state.theta) * params.dt
         y = state.y + action[0] * jnp.sin(state.theta) * params.dt
         theta = state.theta + action[1] * params.dt
         theta = jnp.mod(theta, 2 * jnp.pi)
         
+        # Obstacles state evolution
         obs_state = state.obs_state
         obs_state = obs_state.at[:, 0:2].add(obs_state[:, 2:4] * params.dt)
+        
+        # Rebound if out of bounds
+        out_of_x_bounds = jnp.logical_or(
+            obs_state[:, 0] < params.x_lim[0],
+            obs_state[:, 0] > params.x_lim[1],
+        )
+        out_of_y_bounds = jnp.logical_or(
+            obs_state[:, 1] < params.y_lim[0],
+            obs_state[:, 1] > params.y_lim[1],
+        )
+        obs_state = jax.numpy.where(
+            out_of_x_bounds[:, None],
+            obs_state * jnp.array([1, 1, -1, 1]),
+            obs_state
+        )
+        obs_state = jax.numpy.where(
+            out_of_y_bounds[:, None],
+            obs_state * jnp.array([1, 1, 1, -1]),
+            obs_state
+        )
 
-        # Important: Reward is based on termination is previous step transition
-        reward = -jnp.sqrt(x**2 + y**2)
+        # Reward computation
+        out_of_bounds = jnp.logical_or(
+            jnp.logical_or(x < params.x_lim[0], x > params.x_lim[1]),
+            jnp.logical_or(y < params.y_lim[0], y > params.y_lim[1]),
+        )
+        
+        distance_to_target = jnp.sqrt((x - state.target_state[0])**2 + (y - state.target_state[1])**2)
+        target_reached = distance_to_target < 1.0
+        
+        reward = 0
+        reward = jax.lax.cond(out_of_bounds, lambda _: reward - 100, lambda _: reward, None)
+        reward = jax.lax.cond(target_reached, lambda _: reward + 1000, lambda _: reward, None)
 
         # Update state dict and evaluate termination conditions
         state = EnvState(
@@ -75,6 +99,7 @@ class ClutteredEnv(environment.Environment[EnvState, EnvParams]):
             y=y,
             theta=theta,
             obs_state=obs_state,
+            target_state=state.target_state,
             time=state.time + 1,
         )
         done = self.is_terminal(state, params)
@@ -91,13 +116,18 @@ class ClutteredEnv(environment.Environment[EnvState, EnvParams]):
         self, key: chex.PRNGKey, params: EnvParams
     ) -> Tuple[chex.Array, EnvState]:
         """Performs resetting of environment."""
-
         robot_state = jax.random.uniform(key, minval=0, maxval=1, shape=(3,))
+        target_state = jax.random.uniform(
+            key,
+            minval=jnp.array([params.x_lim[0], params.y_lim[0]]),
+            maxval=jnp.array([params.x_lim[1], params.y_lim[1]]),
+            shape=(2,)
+        )
         obs_state = jax.random.uniform(
             key,
             minval=0,
             maxval=1,
-            shape=(N_OBS, 4)
+            shape=(N_OBSTACLES, 4)
         )
         
         state = EnvState(
@@ -105,36 +135,35 @@ class ClutteredEnv(environment.Environment[EnvState, EnvParams]):
             y=robot_state[1] * (params.y_lim[1] - params.y_lim[0]) + params.y_lim[0],
             theta=robot_state[2] * 2 * jnp.pi,
             obs_state=obs_state,
+            target_state=target_state,
             time=0,
         )
         return self.get_obs(state), state
 
     def get_obs(self, state: EnvState, params=None, key=None) -> chex.Array:
         """Applies observation function to state."""
-        distances = jnp.sqrt((state.obs_state[:, 0] - state.x) ** 2 + (state.obs_state[:, 1] - state.y) ** 2)
-        nearest_obs_idx = jnp.argmin(distances)
-        nearest_obs_pos = state.obs_state[nearest_obs_idx, 0:2]
-        
-        return jnp.array([state.x, state.y, state.theta, nearest_obs_pos[0], nearest_obs_pos[1]])
+        return jnp.array([
+            state.x, state.y, state.theta,
+            state.target_state[0], state.target_state[1],
+        ])
 
     def is_terminal(self, state: EnvState, params: EnvParams) -> jnp.ndarray:
         """Check whether state is terminal."""
-        # Check termination criteria
-        done1 = jnp.logical_or(
-            state.x < -params.x_lim[0],
+        out_of_x_bounds = jnp.logical_or(
+            state.x < params.x_lim[0],
             state.x > params.x_lim[1],
         )
-        done2 = jnp.logical_or(
-            state.y < -params.y_lim[0],
+        out_of_y_bounds = jnp.logical_or(
+            state.y < params.y_lim[0],
             state.y > params.y_lim[1],
         )
-        # done3 = jnp.any(jnp.sqrt((state.obs_state[:, 0] - state.x) ** 2 + (state.obs_state[:, 1] - state.y) ** 2) < 0.1)
-
-        # Check number of steps in episode termination condition
-        done_steps = state.time >= params.max_steps_in_episode
+        out_of_bounds = jnp.logical_or(out_of_x_bounds, out_of_y_bounds)
         
-        # done = jnp.logical_or(jnp.logical_or(jnp.logical_or(done1, done2), done3), done_steps)
-        done = jnp.logical_or(jnp.logical_or(done1, done2), done_steps)
+        target_reached = jnp.sqrt((state.x - state.target_state[0])**2 + (state.y - state.target_state[1])**2) < 1.0
+        
+        time_finished = state.time >= params.max_steps_in_episode
+        
+        done = jnp.logical_or(jnp.logical_or(out_of_bounds, target_reached), time_finished)
         
         return done
 
@@ -146,11 +175,11 @@ class ClutteredEnv(environment.Environment[EnvState, EnvParams]):
     @property
     def num_actions(self) -> int:
         """Number of actions possible in environment."""
-        return 2
+        return N_ACTIONS
 
     def action_space(self, params: Optional[EnvParams] = None) -> spaces.Box:
         """Action space of the environment."""
-        return spaces.Box(-1.0, 1.0, (2,), dtype=jnp.float32)
+        return spaces.Box(-1.0, 1.0, (N_ACTIONS,), dtype=jnp.float32)
 
     def observation_space(self, params: EnvParams) -> spaces.Box:
         """Observation space of the environment."""
@@ -169,30 +198,19 @@ class ClutteredEnv(environment.Environment[EnvState, EnvParams]):
             params.x_lim[1],
             params.y_lim[1],
         ])
-        return spaces.Box(low, high, (5,), dtype=jnp.float32)
+        
+        return spaces.Box(low, high, (N_OBSERVATIONS,), dtype=jnp.float32)
 
     def state_space(self, params: EnvParams) -> spaces.Dict:
         """State space of the environment."""
-        low = jnp.array([
-            params.x_lim[0],
-            params.y_lim[0],
-            0 * jnp.pi,
-        ])
-        
-        high = jnp.array([
-            params.x_lim[1],
-            params.y_lim[1],
-            2 * jnp.pi,
-        ])
-        
         return spaces.Dict({
-            "x": spaces.Box(low[0], high[0], (), jnp.float32),
-            "y": spaces.Box(low[1], high[1], (), jnp.float32),
-            "theta": spaces.Box(low[2], high[2], (), jnp.float32),
+            "x": spaces.Box(params.x_lim[0], params.x_lim[1], (), jnp.float32),
+            "y": spaces.Box(params.y_lim[0], params.y_lim[0], (), jnp.float32),
+            "theta": spaces.Box(0, 2*jnp.pi, (), jnp.float32),
             "obs_state": spaces.Box(
                 [params.x_lim[0], params.y_lim[0], -1, -1],
                 [params.x_lim[1], params.y_lim[1],  1,  1],
-                (N_OBS, 4),
+                (N_OBSTACLES, 4),
                 jnp.float32
             ),
             "time": spaces.Discrete(params.max_steps_in_episode),
